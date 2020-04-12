09d561b767582775c071025836f7de98bcc36e95
Ahmad Marouf <ahmad-marouf@hotmail.com>
2020-04-12 22:41:03 +0200

    Initial commit

---
+++ src/com/GUI/MainMenu.java
@@ -0,0 +1,14 @@
+package com.GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
