42ece35f9dd64bc563860dfe26c8edce7eff1312
Hadeir Osama <hadeir1311@mail.com>
2020-04-21 20:25:28 +0200

    Added creatObject

--- src/com/GUI/MainMenu.java
+++ src/com/GUI/MainMenu.java
@@ -1,5 +1,7 @@
 package com.GUI;
 
+import com.GameControllers.Controller;
+import com.GameControllers.GameObject;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -7,8 +9,15 @@ public class MainMenu extends Application {
 
     public static void main(String[] args) { launch(args); }
 
+    /**
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Controller controller = new Controller();
+        GameObject gameObject = controller.createGameObject();
+        //System.out.println(gameObject.getObjectType().toString());
     }
 }
--- src/com/GameControllers/Controller.java
+++ src/com/GameControllers/Controller.java
@@ -1,10 +1,39 @@
 package com.GameControllers;
 
+import java.util.Random;
+
 public class Controller implements GameActions {
 
     @Override
     public GameObject createGameObject() {
-        return null;
+        Random random = new Random(7);
+        ObjectFactory factory = new ObjectFactory();
+        GameObject gameObject= null;
+        switch (random.nextInt()) {
+            case '1':
+               gameObject = factory.getGameObject(ENUM.APPLE);
+                break;
+            case '2':
+                gameObject = factory.getGameObject(ENUM.ORANGE);
+                break;
+            case '3':
+                gameObject = factory.getGameObject(ENUM.STRAWBERRY);
+                break;
+            case '4':
+                gameObject = factory.getGameObject(ENUM.WATERMELON);
+                break;
+            case '5':
+                gameObject = factory.getGameObject(ENUM.PINEAPPLE);
+                break;
+            case '6':
+                gameObject = factory.getGameObject(ENUM.FATAL);
+                break;
+            case '7':
+                gameObject = factory.getGameObject(ENUM.DANGEROUS);
+                break;
+
+        }
+        return gameObject;
     }
 
     @Override
